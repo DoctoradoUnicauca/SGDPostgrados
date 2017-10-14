524ec6212abd2e4ec435ed124c9d5d8ea672ccdd
andersonbui <andersonbuitron@unicauca.edu.co>
2017-10-14 16:39:19 -0500

    actualizado script BD

--- src/java/co/unicauca/proyectobase/validadores/ValidadorISSN.java
+++ src/java/co/unicauca/proyectobase/validadores/ValidadorISSN.java
@@ -24,25 +24,10 @@ public class ValidadorISSN implements Validator {
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         String issn = value.toString();
-        
-        if (issn.length() != 0) {
-
-            if (!validarFormato(issn)) {
-                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Formato del ISSN es incorrecto, debe ser de 8 numeros o 7 numeros y una X ");
-                throw new ValidatorException(msg);
-            }
+        char ultimaLetra = issn.charAt(issn.length()-1);
+        if (ultimaLetra != 'x' && ultimaLetra != 'X' && !Character.isDigit(ultimaLetra)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "El ISSN deben terminnar en x o numero.");
+            throw new ValidatorException(msg);
         }
-
     }
-
-    //valida el formato del DOI
-    public boolean validarFormato(String doi) {
-        //  Pattern p = Pattern.compile("(^([0-9]{4})+([0-9]{3})+([0-9X]{1}))$");
-        Pattern p = Pattern.compile("(^([0-9]{4})+([0-9]{3})+([0-9X]{1}))$");
-        //http://www.issn.org/es/comprender-el-issn/que-es-el-numero-issn/
-        //https://goo.gl/jQFjlJ
-        Matcher m = p.matcher(doi);
-        return m.find();
-    }
-
 }
